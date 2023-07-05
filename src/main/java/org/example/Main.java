26bf69512f33f2873a059c59ccb03e24a49af5c9
JaimeIngenia <jamoncayop@unal.edu.co>
2023-07-05 11:45:25 -0500

    modificadores de acceso

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+//import org.example.modelos.Usuario;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer edad = 26;
+        // vamos a crear un objeto los objetos son variables especiales
+
+        Universidad obetoUniversidad = new Universidad("Juan");
+
+        //Usuario objetoUsuario = new Usuario( 25,"andres","123",4,"jamoncayop@unal.edu.co" );
+        System.out.println(edad);
+        System.out.println(obetoUniversidad.nombres);
+        //System.out.println(objetoUsuario.id);
+        //System.out.println(objetoUsuario.nombres);
+        //System.out.println(objetoUsuario.documento);
+        //System.out.println(objetoUsuario.ubicacion);
+        //System.out.println(objetoUsuario.correo);
     }
+
+
+
+
 }
---
+++ src/main/java/org/example/Universidad.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Universidad {
+    protected String nombres;
+
+    public Universidad() {
+    }
+
+    public Universidad(String nombres) {
+        this.nombres = nombres;
+    }
+
+}
